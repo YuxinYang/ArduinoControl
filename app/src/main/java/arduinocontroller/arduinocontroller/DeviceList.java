1c1edbda3364bda2382c5edee2456d76796ee628
ChunyuanCai <cagehouhou@gmail.com>
2017-10-26 12:29:27 +0200

    Fix the issue with ACTION_FOUND with BE

--- app/src/main/java/arduinocontroller/arduinocontroller/DeviceList.java
+++ app/src/main/java/arduinocontroller/arduinocontroller/DeviceList.java
@@ -5,7 +5,6 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -27,12 +26,14 @@ import java.util.Set;
 
 public class DeviceList extends AppCompatActivity {
 
-    private static final int REQUEST_COARSE_LOCATION = 123456;
+    public static final String TAG = "DEVICE_LIST";
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final int REQUEST_COARSE_LOCATION = 11;
     //widgets
     ToggleButton btnScan;
     ListView devicelist;
     //Bluetooth
-    private BluetoothAdapter myBluetooth = null;
+    private BluetoothAdapter myBluetooth;
     private Set<BluetoothDevice> devices;
     public static String EXTRA_ADDRESS = "device_address";
     private ArrayAdapter<String> adapter;
@@ -42,56 +43,25 @@ public class DeviceList extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
+    private boolean isLocationCoarsePermissionGranted = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "Start...");
+
         setContentView(R.layout.activity_device_list);
-
-        //Calling widgets
-        btnScan = (ToggleButton)findViewById(R.id.btn_scan);
-        devicelist = (ListView)findViewById(R.id.listView);
-
-        //if the device has bluetooth
-        myBluetooth = BluetoothAdapter.getDefaultAdapter();
-
-        if(myBluetooth == null) {
-            //Show a mensag. that the device has no bluetooth adapter
-            Toast.makeText(getApplicationContext(), "Bluetooth Device Not Available", Toast.LENGTH_LONG).show();
-            finish();
-        }else if(!myBluetooth.isEnabled()) {
-            //Ask to the user turn the bluetooth on
-            Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(turnBTon,1);
-        }
-
-        devices = myBluetooth.getBondedDevices();
-        pairedDevicesList(devices);
-
-        IntentFilter filter=new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        filter.addAction(BluetoothDevice.ACTION_NAME_CHANGED);
-        registerReceiver(bReciever,filter);
-
+        initViews();
+        initBluetoothPermission();
+        getPairedDevices();
         checkLocationPermission();
-
-        btnScan.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    adapter.clear();
-                    myBluetooth.startDiscovery();
-                } else {
-                    //getActivity().unregisterReceiver(bReciever);
-                    myBluetooth.cancelDiscovery();
-                }
-            }
-        });
     }
 
     public void onDestroy() {
         super.onDestroy();
         unregisterReceiver(bReciever);
-        Log.e("destory","Unregister...");
+        Log.i(TAG, "Unregister...");
     }
-
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
@@ -100,8 +70,9 @@ public class DeviceList extends AppCompatActivity {
             case REQUEST_COARSE_LOCATION: {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    isLocationCoarsePermissionGranted = true;
                 } else {
-                    //TODO re-request
+                    // for now just request again
                     checkLocationPermission();
                 }
                 break;
@@ -109,7 +80,46 @@ public class DeviceList extends AppCompatActivity {
         }
     }
 
-    protected void checkLocationPermission() {
+    private void initViews() {
+        //Calling widgets
+        btnScan = (ToggleButton) findViewById(R.id.btn_scan);
+        devicelist = (ListView) findViewById(R.id.listView);
+
+        btnScan.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    adapter.clear();
+                    // start discovering devices only if location coarse permission get granted
+                    if (isLocationCoarsePermissionGranted) {
+                        myBluetooth.startDiscovery();
+                    }
+                } else {
+                    //getActivity().unregisterReceiver(bReciever);
+                    myBluetooth.cancelDiscovery();
+                }
+            }
+        });
+    }
+
+    private void initBluetoothPermission() {
+        //if the device has bluetooth
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
+
+        if (myBluetooth == null) {
+            Toast.makeText(getApplicationContext(), "Bluetooth Device Not Available", Toast.LENGTH_LONG).show();
+            finish();
+        } else if (!myBluetooth.isEnabled()) {
+            Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnBTon, REQUEST_ENABLE_BT);
+        }
+    }
+
+    private void getPairedDevices() {
+        devices = myBluetooth.getBondedDevices();
+        pairedDevicesList(devices);
+    }
+
+    private void checkLocationPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
 
@@ -118,6 +128,7 @@ public class DeviceList extends AppCompatActivity {
                     REQUEST_COARSE_LOCATION);
         }
     }
+
 
     private final BroadcastReceiver bReciever = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
@@ -128,7 +139,7 @@ public class DeviceList extends AppCompatActivity {
                 String deviceItem = device.getName() + "\n" + device.getAddress();
                 Log.e("", String.valueOf(adapter.getPosition(deviceItem)));
                 //show Bluetoothdevices unique
-                if(!(adapter.getPosition(deviceItem)> -1)){
+                if (!(adapter.getPosition(deviceItem) > -1)) {
                     adapter.add(deviceItem);
                 }
                 devicelist.setAdapter(adapter);
@@ -136,28 +147,25 @@ public class DeviceList extends AppCompatActivity {
         }
     };
 
-    private void pairedDevicesList(Set<BluetoothDevice> set)
-    {
+    private void pairedDevicesList(Set<BluetoothDevice> set) {
         ArrayList<String> list = new ArrayList<String>();
 
-        if (devices.size()>0) {
-            for(BluetoothDevice bt : set) {
+        if (devices.size() > 0) {
+            for (BluetoothDevice bt : set) {
                 list.add(bt.getName() + "\n" + bt.getAddress()); //Get the device's name and the address
             }
         } else {
             Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
         }
 
-        adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
         devicelist.setAdapter(adapter);
         devicelist.setOnItemClickListener(myListClickListener); //Method called when the device from the list is clicked
 
     }
 
-    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener()
-    {
-        public void onItemClick (AdapterView<?> av, View v, int arg2, long arg3)
-        {
+    private AdapterView.OnItemClickListener myListClickListener = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView<?> av, View v, int arg2, long arg3) {
             myBluetooth.cancelDiscovery();
             // Get the device MAC address, the last 17 chars in the View
             String info = ((TextView) v).getText().toString();
